012964412585b9e2b1a855ac003dbda1c48af698
Per Steen Olsen <persteen22@hotmail.com>
2021-02-26 21:53:14 +0100

    Adding a Get Request for reveiving the 10 from a test Web API

--- src/app/shared/components/listposts/listposts.component.ts
+++ src/app/shared/components/listposts/listposts.component.ts
@@ -9,30 +9,37 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ListpostsComponent implements OnInit {
+
     totalAngularPackages: any;
+	listOfPosts: any;
 	
     constructor(private http: HttpClient) { }
 
-    ngOnInit() {          
+    ngOnInit() {  
+	    
+		// Requesting a total number of Angular packages at api.nmps.io
         this.http.get<SearchResults>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
-            this.totalAngularPackages = data.total;
+        this.totalAngularPackages = data.total;
         })
 	  
-	  // TEST !
-	  //  this.http.get<NewSearchResults>('https://jsonplaceholder.typicode.com/posts??_start=0&_limit=1').subscribe(data => {
-      //      this.totalAngularPackages = data.userId;
-      //  })
+	    // Requesting a list of posts at jsonplaceholder
+	    this.http.get<NewSearchResults>('https://jsonplaceholder.typicode.com/posts??_start=0&_limit=10').subscribe(data => {
+        this.listOfPosts = data;
+       })
     }
 }
 
+// The interface matching the result from api.nmps.io Web API
 interface SearchResults {
     total: number;
     results: Array<object>;
 }
 
- // TEST !
+// The interface matching the result from jsonplaceholder Web API
 interface NewSearchResults {
-    userId: Array<object>;
-   // title: string;
-    
+    userId: any;
+	id: any;
+    title: any;
+	body: any;
+ 
 }
