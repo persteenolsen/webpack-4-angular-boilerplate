84c33db1a690967d573ce8108f3b64bf658fe59d
Per Steen Olsen <persteen22@hotmail.com>
2021-04-09 15:52:16 +0200

    Refactoring the folder structure of the Angular App to become more simple and clear

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './shared/components/home/home.component';
-import { AboutComponent } from './shared/components/about/about.component';
-import { MyInfoComponent } from './shared/components/myinfo/myinfo.component';
-import { ListpostsComponent } from './shared/components/listposts/listposts.component';
-import { SelectedPostComponent } from './shared/components/selectedpost/selectedpost.component';
-import { EditPostComponent } from './shared/components/editpost/editpost.component';
-import { CreatePostComponent } from './shared/components/createpost/createpost.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MyInfoComponent } from './components/myinfo/myinfo.component';
+import { ListpostsComponent } from './components/listposts/listposts.component';
+import { SelectedPostComponent } from './components/selectedpost/selectedpost.component';
+import { EditPostComponent } from './components/editpost/editpost.component';
+import { CreatePostComponent } from './components/createpost/createpost.component';
 
-import { ErrorComponent } from './shared/components/error/error.component';
+import { ErrorComponent } from './components/error/error.component';
 
 const appRoutes: Routes = [
 
@@ -17,9 +17,6 @@ const appRoutes: Routes = [
 	{ path: 'about', component: AboutComponent },
 	{ path: 'myinfo', component: MyInfoComponent },
 	{ path: 'listposts', component: ListpostsComponent },
-	
-	{ path: 'menu', loadChildren: './modules/menu/menu.module#MenuModule' },
-	
 	{ path: 'thepost/:id', component: SelectedPostComponent },
 	{ path: 'editpost/:id', component: EditPostComponent },
 	{ path: 'createpost', component: CreatePostComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-import { HomeComponent } from './shared/components/home/home.component';
-import { AboutComponent } from './shared/components/about/about.component';
-import { MyInfoComponent } from './shared/components/myinfo/myinfo.component';
-import { ListpostsComponent } from './shared/components/listposts/listposts.component';
-import { SelectedPostComponent } from './shared/components/selectedpost/selectedpost.component';
-import { EditPostComponent } from './shared/components/editpost/editpost.component';
-import { CreatePostComponent } from './shared/components/createpost/createpost.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MyInfoComponent } from './components/myinfo/myinfo.component';
+import { ListpostsComponent } from './components/listposts/listposts.component';
+import { SelectedPostComponent } from './components/selectedpost/selectedpost.component';
+import { EditPostComponent } from './components/editpost/editpost.component';
+import { CreatePostComponent } from './components/createpost/createpost.component';
 
-import { ErrorComponent } from './shared/components/error/error.component';
+import { ErrorComponent } from './components/error/error.component';
 
 import '../assets/css/styles';
 
